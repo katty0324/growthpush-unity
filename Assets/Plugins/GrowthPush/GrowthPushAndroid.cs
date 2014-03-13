2f28b3756428abf2694cbec4ccdba6b2b1e3e41c
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-03-13 11:34:07 +0900

    refacted android receive notification process.

--- Assets/Plugins/GrowthPush/GrowthPushAndroid.cs
+++ Assets/Plugins/GrowthPush/GrowthPushAndroid.cs
@@ -2,136 +2,95 @@ using UnityEngine;
 using System.Collections;
 using System;
 
-
 public class GrowthPushAndroid
 {
-	public enum Environment 
-	{
-		Production, 
-		Development
-	};
-	
-	private static GrowthPushAndroid instance = null;	
-	
-	public static GrowthPushAndroid GetInstance() 
-	{
-		if(instance == null)
-			instance = new GrowthPushAndroid();
-		return instance;
-	}
-#if UNITY_ANDROID && !UNITY_EDITOR	
-	private AndroidJavaObject growthPush = null;	
-#endif
-	
-	private GrowthPushAndroid()
-	{
-#if UNITY_ANDROID && !UNITY_EDITOR
-		using(AndroidJavaClass gpclass = new AndroidJavaClass( "com.growthpush.GrowthPush" ))
-		{
-			growthPush = gpclass.CallStatic<AndroidJavaObject>("getInstance"); 
-		}
-#endif
-	}
+		private static GrowthPushAndroid instance = new GrowthPushAndroid ();
+		#if UNITY_ANDROID && !UNITY_EDITOR
+			private AndroidJavaObject growthPush = null;	
+		#endif
 
-	public GrowthPushAndroid Initialize(int applicationId, string secret, Environment environment, bool debug) 
-	{		
-#if UNITY_ANDROID && !UNITY_EDITOR
-		if( growthPush != null )
+		public static GrowthPushAndroid GetInstance ()
 		{
-			AndroidJavaClass  environmentClass = new AndroidJavaClass("com.growthpush.model.Environment");
-			AndroidJavaObject environmentObject = environmentClass.GetStatic<AndroidJavaObject>(environment == Environment.Production ? "production" : "development");
-			AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        	AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-			growthPush.Call<AndroidJavaObject>("initialize", activity, applicationId, secret, environmentObject, debug);
+
+				#if UNITY_ANDROID && !UNITY_EDITOR
+				if (growthPush == null) {
+					using(AndroidJavaClass gpclass = new AndroidJavaClass( "com.growthpush.GrowthPush" ))
+					{
+						growthPush = gpclass.CallStatic<AndroidJavaObject>("getInstance"); 
+					}
+				}
+				#endif
+
+				return instance;
 		}
-		else
+
+		public GrowthPushAndroid Initialize (int applicationId, string secret, Environment environment, bool debug)
+		{		
+				#if UNITY_ANDROID && !UNITY_EDITOR
+						if( growthPush != null )
+						{
+							AndroidJavaClass  environmentClass = new AndroidJavaClass("com.growthpush.model.Environment");
+							AndroidJavaObject environmentObject = environmentClass.GetStatic<AndroidJavaObject>(environment == Environment.Production ? "production" : "development");
+							AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+				        	AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+							growthPush.Call<AndroidJavaObject>("initialize", activity, applicationId, secret, environmentObject, debug);
+						}
+				#endif
+				return this;
+		}
+
+		public GrowthPushAndroid Register (string senderId)
 		{
-			Debug.LogError( "growthPush is not created.");
+				#if UNITY_ANDROID && !UNITY_EDITOR
+						if( growthPush != null )
+							growthPush.Call<AndroidJavaObject>("register", senderId);
+				#endif
+				return this;
 		}
-#endif
-		return this;
-	}
-	
-	public GrowthPushAndroid Register(string senderId) 
-	{
-#if UNITY_ANDROID && !UNITY_EDITOR
-		if( growthPush != null )
+
+		public void TrackEvent (string name)
 		{
-			growthPush.Call<AndroidJavaObject>("register", senderId);
+				TrackEvent (name, "");
 		}
-		else
+
+		public void TrackEvent (string name, string val)
 		{
-			Debug.LogError( "growthPush is not created.");
+			#if UNITY_ANDROID && !UNITY_EDITOR
+					if( growthPush != null )
+						growthPush.Call("trackEvent", name, val);
+			#endif
 		}
-#endif
-		return this;
-	}
-	
-	public void TrackEvent(string name) 
-	{
-		TrackEvent(name, "");
-	}
-	
-	public void TrackEvent(string name, string val) 
-	{
-#if UNITY_ANDROID && !UNITY_EDITOR
-		if( growthPush != null )
+
+		public void SetTag (string name)
 		{
-			growthPush.Call("trackEvent", name, val);
+				SetTag (name, "");
 		}
-		else
+
+		public void SetTag (string name, string val)
 		{
-			Debug.LogError( "growthPush is not created.");
+			#if UNITY_ANDROID && !UNITY_EDITOR
+					if( growthPush != null )
+						growthPush.Call("setTag", name, val);
+			#endif
 		}
-#endif
-	}
-	
-	public void SetTag(string name) 
-	{
-		SetTag(name, "");
-	}
-		
-	public void SetTag(string name, string val) 
-	{
-#if UNITY_ANDROID && !UNITY_EDITOR
-		if( growthPush != null )
+
+		public void SetDeviceTags ()
 		{
-			growthPush.Call("setTag", name, val);
+			#if UNITY_ANDROID && !UNITY_EDITOR
+					if( growthPush != null )
+						growthPush.Call("setDeviceTags");
+			#endif
 		}
-		else
+
+		public void callTrackGrowthPushMessage ()
 		{
-			Debug.LogError( "growthPush is not created.");
+			#if UNITY_ANDROID && !UNITY_EDITOR
+					using(AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+					{
+			    		AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+						if(activity != null)
+							activity.CallStatic("callTrackGrowthPushMessage");
+					}
+			#endif
 		}
-#endif
-	}
-		
-	public void SetDeviceTags() 
-	{
-#if UNITY_ANDROID && !UNITY_EDITOR
-		if( growthPush != null )
-		{
-			growthPush.Call("setDeviceTags");
-		}
-		else
-		{
-			Debug.LogError( "growthPush is not created.");
-		}
-#endif
-	}
-	
-	public void callTrackGrowthPushMessage()
-	{
-#if UNITY_ANDROID && !UNITY_EDITOR
-		using(AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-		{
-    		AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-			if(activity != null)
-				activity.CallStatic("callTrackGrowthPushMessage");
-		}
-#endif
-	}
 }
-
-
-
-
